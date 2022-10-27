4ec07139288f4d62451114f8699335a5346c7425
Joo <kh.ochir24@gmail.com>
2022-10-27 14:46:06 +0800

    update

--- register.php
+++ register.php
@@ -7,17 +7,29 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
   </head>
   <body>
-   <?php include ='navbar.php';?>
+    <?php include 'registration.php'; ?>
+   <?php include 'navbar.php';?>
    <div class="container">
-   <form>
+   <form action="register.php" method="POST">
+     <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Name</label>
+        <input type="text" required name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+     </div>
+     <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Username</label>
+        <input type="text" required name="Username" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+     </div>
      <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-     <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        <input type="email" required name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
      </div>
      <div class="mb-3">
         <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
+        <input type="password" name="password" required class="form-control" id="exampleInputPassword1">
+    </div>
+    <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password Confirmation</label>
+        <input type="password" name="password_confirmation" required class="form-control" id="exampleInputPassword1">
     </div>
     <div class="mb-3 form-check">
         <input type="checkbox" class="form-check-input" id="exampleCheck1">
