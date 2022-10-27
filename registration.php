fdd26c44e89ffb8a81994167cdad304025aaacad
Joo <kh.ochir24@gmail.com>
2022-10-27 16:30:32 +0800

    update

--- registration.php
+++ registration.php
@@ -1,16 +1,16 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password);
+// // Create connection
+// $conn = new mysqli($servername, $username, $password);
 
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
+// // Check connection
+// if ($conn->connect_error) {
+//   die("Connection failed: " . $conn->connect_error);
+// }
+// echo "Connected successfully";
 
 
 
